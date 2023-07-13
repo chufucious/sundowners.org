2b1e043a89b8c4f97c1a0c810b70592ce2bcad05
Eric Chu <chufucious@gmail.com>
2023-07-12 23:50:26 -0700

    logo in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
+<script>
+  import logo from "$lib/assets/images/logo/sundowners/words.png";
+</script>
+
+<img class="h-auto max-w-xs" alt="sundowners" src={logo} />
+
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background-color: theme(colors.black);
   }
 </style>
