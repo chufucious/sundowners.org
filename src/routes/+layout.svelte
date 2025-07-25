0e88f50b4b72c12a1118347e07be39ad4cee56d5
Eric Chu <chufucious@gmail.com>
2025-07-25 13:25:23 -0700

    added logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
   import sundownerswalking from "$lib/assets/Photos/sundownerswalking.jpg?w=390;768;1366;1920&format=webp&as=srcset";
   import fabricSunrise from "$lib/assets/wax-fabric/sunrise.jpeg?w=300&format=webp";
   import logoLion from "$lib/assets/logo/lion.svg?w=400;900;1200&as=srcset";
+  import logoAndType2025 from "$lib/assets/logo/sundowners-logo-type-2025-solid.png?w=400;900;1200&as=srcset";
 </script>
 
 <svelte:head>
@@ -37,10 +38,10 @@
     />
     <a href="/">
       <img
-        class="w-64 absolute m-auto left-0 right-0 top-12 md:top-16 z-10"
-        srcset={logoMosaic}
-        sizes="400px"
-        alt="sundowners mosaic logo"
+        class="w-[400px] md:w-[600px] absolute m-auto left-0 right-0 top-12 md:top-32 z-10 mix-blend-color-dodge"
+        srcset={logoAndType2025}
+        sizes="800px"
+        alt="sundowners logo"
       /></a
     >
 
